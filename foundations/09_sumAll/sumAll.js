7475f983bb3ea88893214d3d3acd976d7b54adfc
Marcos Barrientos <74696345+MarcosB99@users.noreply.github.com>
2026-08-12 16:29:01 -0600

    Pushing final exercises 8, 9, 10, and 11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    let acc = 0;
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
 
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+
+    for (let i = min; i <= max; i++) {
+        acc += i;
+    }
+
+    return acc;
 };
 
 // Do not edit below this line
