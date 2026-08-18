3c647b5415157c2f6ca3fd52d7f286da94985167
Marcos Barrientos <74696345+MarcosB99@users.noreply.github.com>
2026-08-18 15:06:28 -0600

    Pushing exercises 13, 14, 15, 16

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    const target = Number(n);
+    if (target < 0) {
+        return "OOPS";
+    }
 
+    if (target === 0) {
+        return 0;
+    }
+
+    const fibArr = [1, 1];
+
+    for (let i = 2; i < target; i++) {
+        fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+    }
+
+    return fibArr[target - 1];
 };
 
 // Do not edit below this line
